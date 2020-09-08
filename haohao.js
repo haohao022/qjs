80d34d81f6d55270a7a8b7d030bef99b69f10b39
haohao <hh16231214@buaa.edu.cn>
2020-09-09 01:14:55 +0800

    commit to get a new branch

--- haohao.js
+++ haohao.js
@@ -2,31 +2,50 @@ var hperson = {
     name : "haohao",
     age : "22",
     sayhello : function() {
-        print("hello\n");
+        print("... hello\n");
     }
 };
 
-var harray = ["one", "two", "three", "four", "five"];
-
-var hnewstring = new String("hnewstring\n");
-
-var hstring = "hstring\n";
-
 function hfunc() {
     var hfuncobj = {owner:"hfunc"};
-    print(hfuncobj.owner + "\n");
-    print("this is hfunc output.\n");
+    print("...", hfuncobj.owner, "\n");
+    print("... this is hfunc output.\n");
     return 0;
 }
 
-print(hperson.name + "\n");
-print(hperson.age + "\n");
-hperson.sayhello();
+function main() {
+    var harray0 = ["one", "two", "three", "four", "five"];
+    
+    var hnewstring0 = new String("hnewstring0\n");
+    
+    var hstring0 = "hstring0\n";
+    
+    var harray1 = ["six", "seven", "eight", "nine", "ten"];
 
-print(harray[0], harray[3], "\n");
+    var hnewstring1 = new String("hnewstring1\n");
 
-print(hnewstring);
+    this.hstring1 = "hstring1\n";
 
-print(hstring);
+    // window.hstring2 = "hstring2\n";
 
-hfunc();
+    globalstring = "globalstring\n";
+
+    print("...", hperson.name, "\n");
+    print("...", hperson.age, "\n");
+    hperson.sayhello();
+    
+    print("...", harray0[0], harray0[3], harray1[1], harray1[4], "\n");
+    
+    print("...", hnewstring0, hnewstring1);
+    
+    print("...", hstring0, hstring1);
+    
+    print("...", globalstring);
+
+    hfunc();
+}
+
+main();
+// print("...", hstring2);
+print("...", hstring1);
+print("...", hstring0);
